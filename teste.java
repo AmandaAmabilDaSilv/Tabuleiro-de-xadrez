c9acf0522cb4660882be7736d54999953d92d893
Amanda Amabili da Silva <amanda.s101@aluno.ifsc.edu.br>
2026-09-19 19:50:06 -0300

    Adcionado funcionalidade input de colunas, tamanho do tabuleiro, restrições de números impars

--- teste.java
+++ teste.java
@@ -1,3 +1,24 @@
 void main(){
+    String linha;
+    linha = IO.readln("Quantas Colunas: ");
+    String[] partes = linha.split(" ");
+
+
+    int coluna = Integer.parseInt(partes[0]);
+
+       if(coluna % 2 == 1){
+
+         IO.println("O número de colunas não pode ser impar, porque eu não quero." );
+
+       } else{
+
+        int tabuleiro = coluna * 2;
+
+    IO.println("A quantidade de quadradinhos do tabuleiro com base no número de colunas é: " + tabuleiro);
+
+       };
     
+
+
+
 }
