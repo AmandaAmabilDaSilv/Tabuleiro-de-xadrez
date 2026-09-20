be80f163543993d77309a9f99b7e3ac1f313c8be
Amanda Amabili da Silva <amanda.s101@aluno.ifsc.edu.br>
2026-09-19 21:11:09 -0300

    Feature impressão visual do tabuleiro para o usuario V 0.1

--- teste.java
+++ teste.java
@@ -1,38 +1,57 @@
 void main(){
     String linha;
-    linha = IO.readln("Quantas Colunas: ");
+    linha = IO.readln("Quantas Colunas: "); //pega o número de colunas
     String[] partes = linha.split(" ");
 
     int tabuleiro = 0;
 
-    int coluna = Integer.parseInt(partes[0]);
+    int coluna = Integer.parseInt(partes[0]);  
 
        if(coluna % 2 == 1){
 
          IO.println("O número de colunas não pode ser impar, porque eu não quero." );
+         return; //interrompe o codigo se ele por impar
 
        } else{
 
-         tabuleiro = coluna * 2;
+         tabuleiro = coluna * coluna; //calcula a quantidade de quadrados do tabuleiro
           
     IO.println("A quantidade de quadradinhos do tabuleiro com base no número de colunas é: " + tabuleiro);
          
        };
-      
-    int quadradinhos = tabuleiro;
-    while ( quadradinhos > 0 ){
-         int i = 0;
 
-         IO.println("*");
-         
+    int i = 0; // coluna atual
+
+    int quadradinhos = tabuleiro; //cópia de tabuleiro, porque o while vai diminuindo esse valor
+
+    int row = 0; //linha atual
+
+    while ( quadradinhos > 0 ){
+        
+
+     if((row + i) % 2 == 0)
+        { //linha mais a coluna tipo coluna em 0 e linha em 0, o resto da 0
+          IO.print("■ ");
+    }
+           else{IO.print("O ");
+
+           }
+
+            i = i + 1;
+
+        if(i == coluna){  //verifica se o contador de colunas já chegou no valor que o usuario
+            IO.println(); //botou se sim ele volta pro zero, e insere outra linha
+            i = 0;         
+
+            row = row + 1; //soma 1 a uma linha pra indicar que ta em outra
+        };
+        
+   
          quadradinhos = quadradinhos - 1;
-         i = i + 1;
+         
 
     }
 
    
-    
-
-
 
 }
